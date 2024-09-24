793413726e685fca3f549363cf0bfc9aaa2bcf0c
wutyiaung <harleyqueen150@gmail.com>
2024-09-24 20:48:24 +0630

    final update

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="chat-window">
-        <div class="messages">
-            <div v-for="message in messages" :key="message.id" class="single">
-                <span class="created-at">{{ message.created_at.toDate() }}</span>
+        <div class="messages" ref="msgBox">
+            <div v-for="message in formattedMessages" :key="message.id" class="single">
+                <span class="created-at">{{ message.created_at }}</span>
                 <span class="name">{{ message.name }}</span>
                 <span class="message">{{ message.message }}</span>
             </div>
@@ -11,14 +11,25 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted,computed, onUpdated } from 'vue';
 import { db } from '@/firebase/config';
 import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
+import { formatDistanceToNow } from 'date-fns';
 
 export default {
     setup() {
         const messages = ref([]);
-
+        let msgBox = ref(null);
+        // auto scrolling feature
+        onUpdated( () => {
+            msgBox.value.scrollTop = msgBox.value.scrollHeight;
+        })
+        let formattedMessages = computed( () => {
+            return messages.value.map( (msg) => {
+                let formatTime = formatDistanceToNow(msg.created_at.toDate());
+                return {...msg,created_at:formatTime}
+            })
+        })
         // Fetching messages from Firestore using the modular SDK
         onMounted(() => {
             const q = query(collection(db, "messages"), orderBy("created_at"));
@@ -35,17 +46,17 @@ export default {
         });
 
         // Formatting timestamp
-        const formatDate = (timestamp) => {
-            if (timestamp) {
-                const date = timestamp.toDate();
-                const now = new Date();
-                const minutesAgo = Math.floor((now - date) / (1000 * 60));
-                return minutesAgo === 0 ? 'just now' : `${minutesAgo} mins ago`;
-            }
-            return 'unknown time';
-        };
+        // const formatDate = (timestamp) => {
+        //     if (timestamp) {
+        //         const date = timestamp.toDate();
+        //         const now = new Date();
+        //         const minutesAgo = Math.floor((now - date) / (1000 * 60));
+        //         return minutesAgo === 0 ? 'just now' : `${minutesAgo} mins ago`;
+        //     }
+        //     return 'unknown time';
+        // };
 
-        return { messages, formatDate };
+        return { messages, formattedMessages, msgBox };
     }
 };
 </script>
